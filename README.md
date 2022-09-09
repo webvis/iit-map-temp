32f6d380d3e9261d06fc22c22616ac1da0407b7b
Matteo Abrate <matteo.abrate@gmail.com>
2022-09-09 12:05:39 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# anymapper template
+### Visualize your data as a zoomable map
+
+A template for [anymapper](https://github.com/webvis/anymapper) applications. Please refer to the link for more information.
+
+This template is based on `npm init vite` (`svelte` with `typescript` option).
---
+++ src/vite.config.ts
@@ -0,0 +1,28 @@
+import { defineConfig } from 'vite'
+import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { VitePWA } from 'vite-plugin-pwa'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [svelte(), VitePWA({
+    registerType: 'autoUpdate',
+    workbox: {
+      globPatterns: ['./**/*.{js,css,html,ico,png,svg,json}']
+    },
+    includeAssets: ['icon.svg'],
+      manifest: {
+        name: 'Anymapper Application',
+        short_name: 'AnymapperApp',
+        description: 'Template for anymapper application',
+        theme_color: '#bf4400',
+        icons: [
+          {
+            src: 'icon.svg',
+            sizes: 'any',
+            type: 'image/svg+xml'
+          },
+        ]
+      }
+  })],
+  base: '',
+})
